c3b8afb7c0ecaeaa6d81d9d870ff5b27639c6270
Damian Ruiz <nrt.damian@gmail.com>
2020-02-23 01:32:22 -0500

    App.js(updated) components added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './App.css';
 import Hero from './components/Hero'
 import Navigation from './components/Navigation'
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Puppy from './components/Puppies'
 
 function App() {
   return (
     <main style={{backgroundColor: "red"}}>
       <Hero />
       <Navigation />
+      <AboutMe />
+      <Portfolio />
+      <Contact />
+      <Puppy />
     </main>
   );
 }
