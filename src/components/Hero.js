4d45076c85e079018ecc3922e06536a29d5790bb
Damian Ruiz <nrt.damian@gmail.com>
2020-02-23 19:39:36 -0500

    Hero(updated) initial animation added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,30 @@
 import React from 'react'
 
 function Hero() {
+
+    window.addEventListener("load", () => {
+
+        let helloArr = ["hola", "salut", "nǐ hǎo", "ciao", "konnichiwa", "hallo", "olá", "hello,"];
+        let index = 0;
+        let rotation = setInterval(() => {
+            document.getElementById("rotatingText").innerText = helloArr[index];
+            index++;
+            if (index === helloArr.length) {
+                clearInterval(rotation);
+            }
+        }, 500);
+    })
+
     return (
         <div>
-        <h1>
-            hello,
+            <h1 id="rotatingText">
+                hello,
         </h1>
-        <h1>
-            i am damian
+            <h1>
+                i am damian
         </h1>
-        <p>
-            your friendly neighbourhood web developer
+            <p>
+                your friendly neighbourhood web developer
         </p>
         </div>
     )
