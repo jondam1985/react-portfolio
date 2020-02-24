85aff62dacf5ebbf094f73bc1514c7c01d32f961
Damian Ruiz <nrt.damian@gmail.com>
2020-02-23 19:38:39 -0500

    App(updated) Modals grouped in new component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React from 'react';
+import React, { useState} from 'react';
 import './App.css';
 import Hero from './components/Hero'
 import Navigation from './components/Navigation'
-import AboutMe from './components/AboutMe';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Puppy from './components/Puppies'
+import Modals from './components/Modals';
 
 function App() {
+  const [active, setActive] = useState("unactive");
+
   return (
     <main style={{backgroundColor: "red"}}>
       <Hero />
-      <Navigation />
-      <AboutMe />
-      <Portfolio />
-      <Contact />
-      <Puppy />
+      <Navigation setActive={setActive} />
+      <Modals active={active} setActive={setActive} />
     </main>
   );
 }
