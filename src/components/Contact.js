a03f6f8fa543c9b662249aeb3eb7064e37b129fe
Damian Ruiz <nrt.damian@gmail.com>
2020-03-01 14:55:38 -0500

    Contact modal updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,11 +9,13 @@ function Contact({ active, setActive }) {
 
     if (active === "activecontact") {
         return (
-            <div>
-                <img style={{ height: "50px", width: "50px" }} src="assets/close.svg" alt="close modal" onClick={hideModal} />
-                <h1>contact</h1>
-                <p>So many ways to stay in touch. Please pick your favourite from the selection below. If you want to know more about me, my work or anything else (meaning of life, are crabs immortal?, etc.) just reach out to me and I'll happily get back to you.</p>
-                <ContactMeans />
+            <div className="contact modal-padding">
+                <img className="modal-close" src="assets/close.svg" alt="close modal" onClick={hideModal} />
+                <h1 className="modal-title">contact</h1>
+                <p className="modal-description">So many ways to stay in touch. Please pick your favourite from the selection below. If you want to know more about me, my work or anything else (meaning of life, are crabs immortal?, etc.) just reach out to me and I'll happily get back to you.</p>
+                <div className="modal-icons-group">
+                    <ContactMeans />
+                </div>
             </div>
         )
     }
